deda372a5957e229073eec193edf1c5c34394360
Loic Ovigne <ovigne1@univ-lorraine.fr>
2023-10-18 17:40:25 +0200

    filter

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Game;
 use App\Form\GameType;
+use App\Repository\CategoryRepository;
 use App\Repository\GameRepository;
 use App\Security\Voter\GameVoter;
 use Doctrine\ORM\EntityManagerInterface;
@@ -16,12 +17,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class GameController extends AbstractController
 {
     #[Route('/game')]
-    public function index(GameRepository $gameRepository): Response
+    public function index(
+        GameRepository $gameRepository, 
+        Request $request, 
+        CategoryRepository $categoryRepository
+    ): Response
     {
-        $entities = $gameRepository->findAll(); // SELECT * FROM game
+        $page = (int) $request->get('p', 1);
+        $itemCount = 10;
+
+        // $entities = $gameRepository->findAll(); // SELECT * FROM game
+        $entities = $gameRepository->findFiltered(
+            $request->get('published', 'ALL'),
+            $request->get('search', ''),
+            $request->get('category', 'ALL'),
+            $itemCount,
+            $page
+        );
+
+        $pageCount = ceil($entities->count() / $itemCount);
+
+        $categories = $categoryRepository->findBy(['published' => true]);
 
         return $this->render('game/index.html.twig', [
             'entities' => $entities, // On envois le resultat dans la vue
+            'categories' => $categories,
+            'pageCount'=> $pageCount,
         ]);
     }
 
--- src/Repository/GameRepository.php
+++ src/Repository/GameRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Game;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
 class GameRepository extends ServiceEntityRepository
@@ -11,5 +12,45 @@ class GameRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Game::class);
+    }
+
+    public function findFiltered(
+        string $published = 'ALL', 
+        string $search = '',
+        string $category = 'ALL',
+        int $itemCount = 10,
+        int $page = 1
+    ): Paginator
+    {
+        $offset = ($page - 1) * $itemCount;
+
+        $qb = $this->createQueryBuilder('g')
+            ->addSelect('c, s, a')
+            ->leftJoin('g.category', 'c')
+            ->leftJoin('g.supports', 's')
+            ->leftJoin('g.author', 'a')
+            ->setMaxResults($itemCount) // LIMIT
+            ->setFirstResult($offset) // OFFSET
+        ;
+
+        if ($published !== 'ALL') {
+            $qb->where('g.published = :published')
+                ->setParameter('published', $published === '1')
+            ;
+        }
+
+        if ($search !== '') {
+            $qb->andWhere('g.name LIKE :search')
+                ->setParameter('search', "%$search%")
+            ;
+        }
+
+        if ($category !== 'ALL') {
+            $qb->andWhere('c.id = :category')
+                ->setParameter(':category', $category)
+            ;
+        }
+
+        return new Paginator($qb->getQuery());
     }
 }
